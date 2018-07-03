af072810caeb91e5332c6d3f0ae2ee86708bf56b
n.fraison <n.fraison@criteo.com>
2018-07-03 09:46:50 +0200

    Increase HostStatistics interval from 10 to 30s
    Will reduce number of events by 8%
    
    Change-Id: Icf168657873768dad6cca6fa19ead1089fe93243

--- forwarder/src/main/java/com/criteo/hadoop/garmadon/forwarder/metrics/HostStatistics.java
+++ forwarder/src/main/java/com/criteo/hadoop/garmadon/forwarder/metrics/HostStatistics.java
@@ -13,7 +13,7 @@ public class HostStatistics {
 
     public static void startReport(ForwarderEventSender forwarderEventSender) {
         Conf<JVMStatisticsProtos.JVMStatisticsData, Void, JVMStatisticsProtos.JVMStatisticsData> conf = new Conf<>();
-        int interval = Integer.getInteger("garmadon.machine-statistics.interval", 10);
+        int interval = Integer.getInteger("garmadon.machine-statistics.interval", 30);
         conf.setInterval(Duration.ofSeconds(interval));
 
         conf.setLogJVMStats(forwarderEventSender::sendAsync);
