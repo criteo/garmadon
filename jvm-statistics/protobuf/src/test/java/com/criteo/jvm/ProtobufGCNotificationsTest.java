f967b53473ca98c2a7df81ef90e8e3e19717bb55
n.fraison <n.fraison@criteo.com>
2018-11-18 15:13:18 +0100

    Correct flaky jvm statistics test

--- jvm-statistics/protobuf/src/test/java/com/criteo/jvm/ProtobufGCNotificationsTest.java
+++ jvm-statistics/protobuf/src/test/java/com/criteo/jvm/ProtobufGCNotificationsTest.java
@@ -1,5 +1,8 @@
 package com.criteo.jvm;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.MessageOrBuilder;
+import com.google.protobuf.util.JsonFormat;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,27 +15,38 @@ import static org.hamcrest.text.MatchesPattern.matchesPattern;
 
 public class ProtobufGCNotificationsTest {
 
-    private static final Pattern GC_PATTERN = Pattern.compile("collector_name: \".*\"\\s+" +
-            "pause_time: \\d+\\s+" +
-            "cause: \"System.gc\\(\\)\"\\s+" +
-            ".*" +
-            "old_after: \\d+\\s+" +
-            "code_before: \\d+\\s+" +
-            "code_after: \\d+\\s+" +
-            "metaspace_before: \\d+\\s+" +
-            "metaspace_after: \\d+\\s+", Pattern.DOTALL);
+    private static final Pattern GC_PATTERN = Pattern.compile("\\{\\s+\"collector_name\":\\s+\".*\"," +
+            "\\s+\"pause_time\":\\s+\"\\d+\"," +
+            "\\s+\"cause\":\\s+\"System\\.gc\\(\\)\"," +
+            "\\s+\"eden_before\":\\s+\"\\d+\"," +
+            "\\s+\"eden_after\":\\s+\"\\d+\"," +
+            "\\s+\"survivor_before\":\\s+\"\\d+\"," +
+            "\\s+\"survivor_after\":\\s+\"\\d+\"," +
+            "\\s+\"old_before\":\\s+\"\\d+\"," +
+            "\\s+\"old_after\":\\s+\"\\d+\"," +
+            "\\s+\"code_before\":\\s+\"\\d+\"," +
+            "\\s+\"code_after\":\\s+\"\\d+\"," +
+            "\\s+\"metaspace_before\":\\s+\"\\d+\"," +
+            "\\s+\"metaspace_after\":\\s+\"\\d+\"\\s+\\}", Pattern.DOTALL);
 
     @Test
     public void getGCNotificationWithInfos() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
         ProtobufGCNotifications notif = new ProtobufGCNotifications();
         notif.subscribe((timestamp, stats) -> {
-            String s = stats.toString();
-            System.out.println(s);
-            assertThat(s, matchesPattern(GC_PATTERN));
-            latch.countDown();
+            String s = null;
+            JsonFormat.Printer printer = JsonFormat.printer()
+                    .includingDefaultValueFields()
+                    .preservingProtoFieldNames();
+            try {
+                s = printer.print((MessageOrBuilder) stats);
+                assertThat(s, matchesPattern(GC_PATTERN));
+                latch.countDown();
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
         });
         System.gc();
-        Assert.assertTrue(latch.await(200, TimeUnit.MILLISECONDS));
+        Assert.assertTrue(latch.await(1000, TimeUnit.MILLISECONDS));
     }
 }
