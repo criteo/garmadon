c764d7b545b69a5f2bc83149059ceddad83bd9d1
n.fraison <n.fraison@criteo.com>
2018-12-12 20:29:04 +0100

    Update garmadon elasticsearch template at reader startup

---
+++ readers/elasticsearch/src/main/java/com/criteo/hadoop/garmadon/elasticsearch/GarmadonEsException.java
@@ -0,0 +1,7 @@
+package com.criteo.hadoop.garmadon.elasticsearch;
+
+public class GarmadonEsException extends Exception {
+    public GarmadonEsException(String message) {
+        super(message);
+    }
+}
