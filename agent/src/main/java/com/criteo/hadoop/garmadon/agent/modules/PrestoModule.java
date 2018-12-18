a5a0037053f102b0a94cd093287ccc321a95a756
n.fraison <n.fraison@criteo.com>
2018-12-18 12:31:58 +0100

    Remove usage of executor pool on presto module

--- agent/src/main/java/com/criteo/hadoop/garmadon/agent/modules/PrestoModule.java
+++ agent/src/main/java/com/criteo/hadoop/garmadon/agent/modules/PrestoModule.java
@@ -7,35 +7,18 @@ import com.criteo.hadoop.garmadon.agent.tracers.jvm.JVMStatisticsTracer;
 import com.criteo.hadoop.garmadon.agent.tracers.presto.PluginClassLoaderTracer;
 
 import java.lang.instrument.Instrumentation;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class PrestoModule implements GarmadonAgentModule {
 
     @Override
     public void setup(Instrumentation instrumentation, AsyncEventProcessor eventProcessor) {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
         // JVM/GC metrics/events
-        executorService.submit(() -> JVMStatisticsTracer.setup(
-                (timestamp, event) -> eventProcessor.offer(timestamp, StandaloneHeader.getInstance().getHeader(), event)));
+        JVMStatisticsTracer.setup((timestamp, event) ->
+                eventProcessor.offer(timestamp, StandaloneHeader.getInstance().getHeader(), event));
 
         // Byte code instrumentation
-        executorService.submit(() -> PluginClassLoaderTracer.setup(instrumentation));
-        executorService.submit(() -> FileSystemTracer.setup(instrumentation,
-                (timestamp, event) -> eventProcessor.offer(timestamp, StandaloneHeader.getInstance().getHeader(), event)));
-
-        executorService.shutdown();
-        // We wait 3 sec executor to instrument classes
-        // If all classes are still not instrumented after that time we let the JVM continue startup
-        // in order to not block the container for too long on agent initialization
-        // Currently we are seeing avg duration of 160 s on MapRed and 6500 s on SPARK
-        // so we consider 3s as a reasonable duration
-        // Downside: we can have some classes not instrumenting loaded by the JVM so missing
-        // some metrics on a container
-        try {
-            executorService.awaitTermination(3000, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException ignored) {
-        }
+        PluginClassLoaderTracer.setup(instrumentation);
+        FileSystemTracer.setup(instrumentation, (timestamp, event) ->
+                eventProcessor.offer(timestamp, StandaloneHeader.getInstance().getHeader(), event));
     }
 }
