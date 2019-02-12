02235f252a7ff7c5569839d3099ccf604f885ada
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2019-02-12 10:43:50 +0100

    normalize uri, src_path and dst_path in reader-common
    move UriHelpre and UriHelperTest to reader common
    update readme

---
+++ readers/common/src/test/java/com/criteo/hadoop/garmadon/reader/GarmadonMessageTest.java
@@ -0,0 +1,73 @@
+package com.criteo.hadoop.garmadon.reader;
+
+import com.criteo.hadoop.garmadon.event.proto.DataAccessEventProtos;
+import com.criteo.hadoop.garmadon.event.proto.EventHeaderProtos;
+import com.criteo.hadoop.garmadon.schema.serialization.GarmadonSerialization;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GarmadonMessageTest {
+
+    @Test
+    public void testToMapNormalized() {
+        long timestampMillis = System.currentTimeMillis();
+        EventHeaderProtos.Header header = EventHeaderProtos.Header.newBuilder().build();
+        DataAccessEventProtos.FsEvent.Builder fsEvent = DataAccessEventProtos.FsEvent.newBuilder()
+                .setSrcPath("hdfs://root/src_file")
+                .setDstPath("hdfs://root/dst_file")
+                .setAction("FS_RENAME")
+                .setHdfsUser("mathieu")
+                .setUri("hdfs://root");
+
+        GarmadonMessage message = new GarmadonMessage(GarmadonSerialization.TypeMarker.FS_EVENT, timestampMillis, header, fsEvent.build(), null);
+
+        Map<String, Object> map = message.toMap(false, false);
+
+        HashMap<Object, Object> expected = new HashMap<>();
+
+        // scheme and host removed
+        expected.put("src_path", "/src_file");
+        expected.put("dst_path", "/dst_file");
+        // normalized uri
+        expected.put("uri", "hdfs://preprod-pa4");
+
+        expected.put("hdfs_user", "mathieu");
+        expected.put("action", "FS_RENAME");
+        expected.put("timestamp", timestampMillis);
+
+        TestCase.assertEquals(expected, map);
+    }
+
+    @Test
+    public void testToMapNormalizedWithPortInfo() {
+        long timestampMillis = System.currentTimeMillis();
+        EventHeaderProtos.Header header = EventHeaderProtos.Header.newBuilder().build();
+        DataAccessEventProtos.FsEvent.Builder fsEvent = DataAccessEventProtos.FsEvent.newBuilder()
+                .setSrcPath("hdfs://root:8020/src_file")
+                .setDstPath("hdfs://root:8020/dst_file")
+                .setAction("FS_RENAME")
+                .setHdfsUser("mathieu")
+                .setUri("hdfs://root:8020");
+
+        GarmadonMessage message = new GarmadonMessage(GarmadonSerialization.TypeMarker.FS_EVENT, timestampMillis, header, fsEvent.build(), null);
+
+        Map<String, Object> map = message.toMap(false, false);
+
+        HashMap<Object, Object> expected = new HashMap<>();
+
+        // scheme and host removed
+        expected.put("src_path", "/src_file");
+        expected.put("dst_path", "/dst_file");
+        // normalized uri
+        expected.put("uri", "hdfs://preprod-pa4");
+
+        expected.put("hdfs_user", "mathieu");
+        expected.put("action", "FS_RENAME");
+        expected.put("timestamp", timestampMillis);
+
+        TestCase.assertEquals(expected, map);
+    }
+}
