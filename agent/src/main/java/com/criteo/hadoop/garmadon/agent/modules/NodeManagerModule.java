2f61a1a674189d268492cfa61e3b551391d89e28
n.fraison <n.fraison@criteo.com>
2019-04-02 14:23:18 +0200

    Add FsTracer to ResourrceManager and Nodemanager

--- agent/src/main/java/com/criteo/hadoop/garmadon/agent/modules/NodeManagerModule.java
+++ agent/src/main/java/com/criteo/hadoop/garmadon/agent/modules/NodeManagerModule.java
@@ -2,6 +2,7 @@ package com.criteo.hadoop.garmadon.agent.modules;
 
 import com.criteo.hadoop.garmadon.agent.AsyncEventProcessor;
 import com.criteo.hadoop.garmadon.agent.headers.NodemanagerHeader;
+import com.criteo.hadoop.garmadon.agent.tracers.hadoop.hdfs.FileSystemTracer;
 import com.criteo.hadoop.garmadon.agent.tracers.hadoop.nodemanager.ContainerResourceMonitoringTracer;
 import com.criteo.hadoop.garmadon.agent.tracers.jvm.JVMStatisticsTracer;
 
@@ -12,6 +13,9 @@ public class NodeManagerModule implements GarmadonAgentModule {
     public void setup(Instrumentation instrumentation, AsyncEventProcessor eventProcessor) {
         JVMStatisticsTracer.setup((timestamp, event) ->
             eventProcessor.offer(timestamp, NodemanagerHeader.getInstance().getBaseHeader(), event));
+
+        FileSystemTracer.setup(instrumentation,
+            (timestamp, event) -> eventProcessor.offer(timestamp, NodemanagerHeader.getInstance().getBaseHeader(), event));
 
         ContainerResourceMonitoringTracer.setup(NodemanagerHeader.getInstance().getBaseHeader(),
                 instrumentation, eventProcessor);
