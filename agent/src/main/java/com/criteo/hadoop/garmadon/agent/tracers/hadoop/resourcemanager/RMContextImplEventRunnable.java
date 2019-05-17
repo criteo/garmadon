801a075162b549a038140599c5bc9ac235626ba8
n.fraison <n.fraison@criteo.com>
2019-05-17 18:06:29 +0200

    Resolve null pointer exception leading to generate partial APPLICATION_EVENT from RM

--- agent/src/main/java/com/criteo/hadoop/garmadon/agent/tracers/hadoop/resourcemanager/RMContextImplEventRunnable.java
+++ agent/src/main/java/com/criteo/hadoop/garmadon/agent/tracers/hadoop/resourcemanager/RMContextImplEventRunnable.java
@@ -5,21 +5,31 @@ import com.criteo.hadoop.garmadon.event.proto.ResourceManagerEventProtos;
 import com.criteo.hadoop.garmadon.schema.events.Header;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.api.records.Container;
 import org.apache.hadoop.yarn.server.resourcemanager.RMContextImpl;
+import org.apache.hadoop.yarn.server.resourcemanager.rmapp.RMApp;
 import org.apache.hadoop.yarn.server.resourcemanager.rmapp.RMAppState;
+import org.apache.hadoop.yarn.server.resourcemanager.rmapp.attempt.RMAppAttempt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class RMContextImplEventRunnable implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RMContextImplEventRunnable.class);
+
     // Cache used to avoid sending FINISHED/KILLED state event multiple times
     // as we iterate on all apps referenced by RM even finished one until they are
     // evicted by the RM
     private final Cache<String, String> cacheFinishedApp = CacheBuilder.newBuilder()
-            .expireAfterAccess(1, TimeUnit.MINUTES)
-            .build();
+        .expireAfterAccess(1, TimeUnit.MINUTES)
+        .build();
 
     private final RMContextImpl rmContext;
     private final TriConsumer<Long, Header, Object> eventHandler;
+
 
     public RMContextImplEventRunnable(RMContextImpl rmContext, TriConsumer<Long, Header, Object> eventHandler) {
         this.rmContext = rmContext;
@@ -30,38 +40,56 @@ public class RMContextImplEventRunnable implements Runnable {
         return !trackingUrl.contains("http://") && !trackingUrl.contains("https://") ? "http://" + trackingUrl : trackingUrl;
     }
 
+    public void sendAppEvent(ApplicationId applicationId, RMApp rmApp) {
+        if (cacheFinishedApp.getIfPresent(applicationId.toString()) == null) {
+            Header.Builder headerBuilder = Header.newBuilder()
+                .withId(applicationId.toString())
+                .withApplicationID(applicationId.toString())
+                .withUser(rmApp.getUser())
+                .withApplicationName(rmApp.getName())
+                .withFramework(rmApp.getApplicationType().toUpperCase());
+
+            RMAppAttempt rmAppAttempt = rmApp.getCurrentAppAttempt();
+            if (rmAppAttempt != null) {
+                headerBuilder.withAttemptID(rmAppAttempt.getAppAttemptId().toString());
+            }
+
+            ResourceManagerEventProtos.ApplicationEvent.Builder eventBuilder = ResourceManagerEventProtos.ApplicationEvent.newBuilder()
+                .setState(rmApp.getState().name())
+                .setQueue(rmApp.getQueue())
+                .addAllYarnTags(rmApp.getApplicationTags());
+
+            Container container = rmApp.getCurrentAppAttempt().getMasterContainer();
+            if (container != null) {
+                eventBuilder.setAmContainerId(container.getId().toString());
+            }
+
+            if (rmApp.getTrackingUrl() != null) {
+                eventBuilder.setTrackingUrl(normalizeTrackingUrl(rmApp.getTrackingUrl()));
+            }
+
+            if (rmApp.getOriginalTrackingUrl() != null && !"N/A".equals(rmApp.getOriginalTrackingUrl())) {
+                eventBuilder.setOriginalTrackingUrl(normalizeTrackingUrl(rmApp.getOriginalTrackingUrl()));
+            }
+
+            eventHandler.accept(System.currentTimeMillis(), headerBuilder.build(), eventBuilder.build());
+
+            if (rmApp.getState() == RMAppState.FINISHED || rmApp.getState() == RMAppState.KILLED) {
+                cacheFinishedApp.put(applicationId.toString(), rmApp.getState().name());
+            }
+        }
+    }
+
     @Override
     public void run() {
         try {
-            if (rmContext != null) {
+            if (rmContext != null && rmContext.getRMApps() != null) {
                 // Get Apps info
                 rmContext.getRMApps().forEach((applicationId, rmApp) -> {
-                    if (cacheFinishedApp.getIfPresent(applicationId.toString()) == null) {
-                        Header header = Header.newBuilder()
-                                .withId(applicationId.toString())
-                                .withApplicationID(applicationId.toString())
-                                .withUser(rmApp.getUser())
-                                .withApplicationName(rmApp.getName())
-                                .withAttemptID(rmApp.getCurrentAppAttempt().getAppAttemptId().toString())
-                                .withFramework(rmApp.getApplicationType().toUpperCase())
-                                .build();
-
-                        ResourceManagerEventProtos.ApplicationEvent.Builder eventBuilder = ResourceManagerEventProtos.ApplicationEvent.newBuilder()
-                                .setState(rmApp.getState().name())
-                                .setQueue(rmApp.getQueue())
-                                .addAllYarnTags(rmApp.getApplicationTags())
-                                .setAmContainerId(rmApp.getCurrentAppAttempt().getMasterContainer().getId().toString())
-                                .setTrackingUrl(normalizeTrackingUrl(rmApp.getTrackingUrl()));
-
-                        if (!"N/A".equals(rmApp.getOriginalTrackingUrl())) {
-                            eventBuilder.setOriginalTrackingUrl(normalizeTrackingUrl(rmApp.getOriginalTrackingUrl()));
-                        }
-
-                        eventHandler.accept(System.currentTimeMillis(), header, eventBuilder.build());
-
-                        if (rmApp.getState() == RMAppState.FINISHED || rmApp.getState() == RMAppState.KILLED) {
-                            cacheFinishedApp.put(applicationId.toString(), rmApp.getState().name());
-                        }
+                    try {
+                        sendAppEvent(applicationId, rmApp);
+                    } catch (Exception ex) {
+                        LOGGER.error("Failed to generate APPLICATION_EVENT for " + applicationId, ex);
                     }
                 });
             }
